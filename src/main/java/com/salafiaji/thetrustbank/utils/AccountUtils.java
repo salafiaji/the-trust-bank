893355f52686d9fdf7605cad35790565c17ae598
salafiaji <salafiaji@gmail.com>
2024-03-21 11:28:05 +0100

    Worked on the service, repository, dto, util layer to produce

--- src/main/java/com/salafiaji/thetrustbank/utils/AccountUtils.java
+++ src/main/java/com/salafiaji/thetrustbank/utils/AccountUtils.java
@@ -5,7 +5,7 @@ import java.time.Year;
 public class AccountUtils {
 
     public static final String ACCOUNT_EXISTS_CODE = "001";
-    public static final String ACCOUNT_EXISTS_MESSAGE = "This is user already has an account created";
+    public static final String ACCOUNT_EXISTS_MESSAGE = "This user already has an account created";
     public static final String ACCOUNT_CREATION_SUCCESS = "002";
     public static final String ACCOUNT_CREATION_MESSAGE = "Account Created successfully!";
     public static String generateAccountNumber() {
